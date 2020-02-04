a4ec77538c917474e4c120c07b87123d148f9911
Zbigniew Karolczuk <zkarolczuk@prescientco.com>
2020-02-04 17:16:06 +0100

    Practicing arrays & system preferences

--- c#_fundamentals/gradebook/src/GradeBook/Program.cs
+++ c#_fundamentals/gradebook/src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -6,7 +7,30 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"Good morning, {args[0]} !");
+            var grades = new List<double>() { 34.1, 10.3, 7.1, 4.2, 17.99 };
+            grades.Add(56.1);
+
+            if (args.Length > 0)
+            {
+                Console.WriteLine($"Good morning, {args[0]} !");
+                System.Console.WriteLine($"The average grade is {avgArray(grades):N1}");
+            }
+            else
+            {
+                Console.WriteLine("Hello User!");
+            }
+        }
+
+        public static double avgArray(List<double> array)
+        {
+            var result = 0.0;
+            foreach (var number in array)
+            {
+                result += number;
+            }
+
+            result /= array.Count;
+            return result;
         }
     }
 }
