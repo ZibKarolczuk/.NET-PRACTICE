a14585510d6cf9e9982f06e2c1291f96d15720f8
Zbigniew Karolczuk <zkarolczuk@prescientco.com>
2020-02-05 12:49:14 +0100

    loops (foreach, do-while, while, for)

--- c#_fundamentals/gradebook/src/GradeBook/Book.cs
+++ c#_fundamentals/gradebook/src/GradeBook/Book.cs
@@ -22,7 +22,13 @@ namespace GradeBook
         {
             // SORT OF VALIDATION
             if (grade >= 0 && grade <= 100)
+            {
                 this.grades.Add(grade);
+            }
+            else
+            {
+                System.Console.WriteLine("Invalid value");
+            }
         }
 
         public Statistics GetStatistics()
@@ -32,11 +38,48 @@ namespace GradeBook
             result.High = double.MinValue;
             result.Low = double.MaxValue;
 
-            foreach (var grade in this.grades)
+            // PRACTICE SAME LOGIC WITH FOREACH LOOP:
+
+            // foreach (var grade in this.grades)
+            // {
+            //     result.Average += grade;
+            //     result.Low = Math.Min(result.Low, grade);
+            //     result.High = Math.Max(result.High, grade);
+            // }
+
+
+            // PRACTICE SAME LOGIC WITH DO-WHILE LOOP:
+
+            // var index = 0;
+            // do
+            // {
+            //     result.Average += grades[index];
+            //     result.Low = Math.Min(result.Low, grades[index]);
+            //     result.High = Math.Max(result.High, grades[index]);
+            //     index++;
+            // } while (index < grades.Count);
+
+
+            // PRACTICE SAME LOGIC WITH WHILE LOOP:
+
+            // var index = 0;
+
+            // while (index < grades.Count)
+            // {
+            //     result.Average += grades[index];
+            //     result.Low = Math.Min(result.Low, grades[index]);
+            //     result.High = Math.Max(result.High, grades[index]);
+            //     index++;
+            // }
+
+
+            // PRACTICE SAME LOGIC WITH FOR LOOP:
+
+            for (var index = 0; index < grades.Count; index++)
             {
-                result.Average += grade;
-                result.Low = Math.Min(result.Low, grade);
-                result.High = Math.Max(result.High, grade);
+                result.Average += grades[index];
+                result.Low = Math.Min(result.Low, grades[index]);
+                result.High = Math.Max(result.High, grades[index]);
             }
 
             // SORT OF VALIDATION - AVOID DIVISION BY ZERO
