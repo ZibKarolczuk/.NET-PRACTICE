f1f28c000fd1ea5fdff9c75def79fff5600379b1
Zbigniew Karolczuk <karolczuk.z@gmail.com>
2020-02-06 23:03:36 +0100

    New training initialized

---
+++ pluralsight_beginnings_c#_collections/src/DaysOfWeek/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DaysOfWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string monday = "monday";
+
+            string[] daysOfWeek = {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+        }
+    }
+}
