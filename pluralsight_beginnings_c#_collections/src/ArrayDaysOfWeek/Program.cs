dde8b3f14e120c609f2fb5e7683a3aa594fd05f4
Zbigniew Karolczuk <karolczuk.z@gmail.com>
2020-02-06 23:27:04 +0100

    Changed the structure

---
+++ pluralsight_beginnings_c#_collections/src/ArrayDaysOfWeek/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ArrayDaysOfWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] daysOfWeek = {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+
+            // foreach (var day in daysOfWeek)
+            // {
+            //     System.Console.WriteLine(day.ToUpper());
+            // }
+
+            System.Console.Write("Write n'th number of week [1 - form Monday]: ");
+
+            var line = System.Console.ReadLine();
+            var index = int.Parse(line) - 1;
+
+            System.Console.WriteLine($"You have selected {daysOfWeek[index]}");
+        }
+    }
+}
