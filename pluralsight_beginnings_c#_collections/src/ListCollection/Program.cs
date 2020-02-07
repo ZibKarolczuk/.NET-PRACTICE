eaf12f4b8cbb6dcd49909a14e0f70c8ce0dabcc3
Zbigniew Karolczuk <karolczuk.z@gmail.com>
2020-02-07 23:47:42 +0100

    initialized ListCollection console app

---
+++ pluralsight_beginnings_c#_collections/src/ListCollection/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ListCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
