b550a37311ea9037d867d58be4a4c89f13f23c8c
Zbigniew Karolczuk <karolczuk.z@gmail.com>
2020-02-09 14:02:45 +0100

    Exercises on modifing a list

---
+++ pluralsight_beginnings_c#_collections/src/ListManipulatingData/NOTES.md
@@ -0,0 +1,11 @@
+Some notes in regards to Collections:
+Arrays / List / Dictionary
+
+Array is fixed size
+List is Dynamic
+Dictionary is a bag with objects so therefor is much faster
+
+Foreach in c# is readonly, c# doesn't give an access to index!
+In order to modify a list (insert or remove) - DO IT BACKWARDS in for loop!
+
+Check out for more methods for List<T> on Microsoft .Net Documentation!
--- pluralsight_beginnings_c#_collections/src/ListManipulatingData/Program.cs
+++ pluralsight_beginnings_c#_collections/src/ListManipulatingData/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ListManipulatingData
 {
@@ -6,7 +7,47 @@ namespace ListManipulatingData
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // List<string> list = new List<string>();
+            var list = new List<string>();
+
+            list.Add("Hello");
+            list.Add("beautiful");
+            list.Add("World");
+            list.Add("from");
+            list.Add("Zibi");
+            list.Add("or");
+            list.Add("odeon");
+
+            //ENUMARATING DATA
+            for (var i = 0; i < list.Count; i++)
+            {
+                System.Console.WriteLine(list[i].ToLower());
+            }
+
+            System.Console.WriteLine("..");
+
+            //ENUMARATING DATA BACKWARDS
+            for (var i = list.Count - 1; i >= 0; i--)
+            {
+                System.Console.WriteLine(list[i].ToUpper());
+            }
+
+            System.Console.WriteLine("..");
+
+            //REMOVING ITEM FROM LIST MUST BE PERFORMED BACKWARDS!!!
+            //INSERTING ITEM IS ALSO BETTER TO PERFORMED BACKWARDS!!!
+            // for (var i = list.Count - 1; i >= 0; i--)
+            // {
+            //     if (list[i].Contains("o")) list.RemoveAt(i);
+            // }
+
+            //OR SUPLIER BY REMOVEALL()
+            list.RemoveAll(x => x.Contains("o"));
+
+            for (var i = 0; i < list.Count; i++)
+            {
+                System.Console.WriteLine(list[i].ToLowerInvariant());
+            }
         }
     }
 }
