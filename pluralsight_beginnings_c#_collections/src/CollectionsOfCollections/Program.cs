0aac4c9ea32d87ad034917627d126c5d8fb10e0b
Zbigniew Karolczuk <karolczuk.z@gmail.com>
2020-02-09 18:23:00 +0100

    New project CollectionsOfCollections

---
+++ pluralsight_beginnings_c#_collections/src/CollectionsOfCollections/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CollectionsOfCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
