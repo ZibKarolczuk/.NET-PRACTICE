05f9a52ca7c99624217c1e61e11f6ba8b458aefe
Zbigniew Karolczuk <karolczuk.z@gmail.com>
2020-02-09 23:33:14 +0100

    OOP fundamentals course - set up the project & some implementations

---
+++ pluralsight_oop_fundamentals_in_c#/ACM/ACM.BL/Customer.cs
@@ -0,0 +1,34 @@
+﻿namespace ACM.BL
+{
+	public class Customer
+	{
+		public int CustomerId { get; private set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string EmailAddress { get; set; }
+
+		public string FullName
+		{
+			get
+			{
+				string fullName = LastName;
+				if (!string.IsNullOrWhiteSpace(FirstName))
+				{
+					if (!string.IsNullOrWhiteSpace(fullName))
+					{
+						fullName += ", ";
+					}
+					fullName += FirstName;
+				}
+				return fullName;
+			}
+		}
+
+		public static int InstanceCount { get; private set; }
+
+		public Customer()
+		{
+			Customer.InstanceCount++;
+		}
+	}
+}
---
+++ pluralsight_oop_fundamentals_in_c#/ACM/Tests/ACM.BLTest/CustomerTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using ACM.BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ACM.BLTest
+{
+	[TestClass]
+	public class CustomerTest
+	{
+		[TestMethod]
+		public void FullNameTestValid()
+		{
+			//Arrange
+			var customer = new Customer { FirstName = "Zbigniew", LastName = "Karolczuk" };
+			string expected = "Karolczuk, Zbigniew";
+
+			//Act
+			string actual = customer.FullName;
+
+			//Assert
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void FullNameLastNameEmptyTestValid()
+		{
+			//Arrange
+			var customer = new Customer { FirstName = "Zbigniew" };
+			string expected = "Zbigniew";
+
+			//Act
+			string actual = customer.FullName;
+
+			//Assert
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void FullNameFirstNameEmptyTestValid()
+		{
+			//Arrange
+			var customer = new Customer { LastName = "Karolczuk" };
+			string expected = "Karolczuk";
+
+			//Act
+			string actual = customer.FullName;
+
+			//Assert
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void InstanceCountTestValid()
+		{
+			//Arrange
+			var customer1 = new Customer();
+			var customer2 = new Customer();
+			var customer3 = new Customer();
+
+			int expected = 3;
+
+			//Act
+			int actual = Customer.InstanceCount;
+
+			//Assert
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
