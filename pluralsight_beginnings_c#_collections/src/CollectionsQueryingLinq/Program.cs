b441284438c0efae32c7fb0f093f886d808ba4ab
Zbigniew Karolczuk <karolczuk.z@gmail.com>
2020-02-09 14:53:52 +0100

    New project Collections Linq

---
+++ pluralsight_beginnings_c#_collections/src/CollectionsQueryingLinq/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CollectionsQueryingLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
