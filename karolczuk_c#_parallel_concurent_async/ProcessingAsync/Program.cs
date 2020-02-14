f3beb35defadf746b4dd84774da32f46abfd1889
Zbigniew Karolczuk <karolczuk.z@gmail.com>
2020-02-14 22:12:13 +0100

    simple async using Task.Run()

--- karolczuk_c#_parallel_concurent_async/ProcessingAsync/Program.cs
+++ karolczuk_c#_parallel_concurent_async/ProcessingAsync/Program.cs
@@ -7,36 +7,9 @@ namespace ProcessingAsync
 {
     class Program
     {
-        static async Task Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            var x = PrintSinOfAngleAsync();
-            Console.WriteLine(await x);
-            Console.WriteLine("Hello");
-            //PrintSinOfAngleAsync2();
-
-            //Task.WhenAll();
-        }
-
-        public static async Task<string> PrintSinOfAngleAsync()
-        {
-            //for (var i = 0; i< 181; i++)
-            //{
-                Thread.Sleep(1000);
-            //Console.WriteLine($"Sine of angle {i} is {Math.Sin((i * Math.PI) / 180)}");
-            return "Jarek Kaczynski";
-
-            //};
-
-        }
-
-        public static async Task PrintSinOfAngleAsync2()
-        {
-            for (var i = 0; i < 181; i++)
-            {
-                Thread.Sleep(30);
-                Console.WriteLine($"Cosine of angle {i} is {Math.Cos((i * Math.PI) / 180)}");
-            };
-
+            await SineCosineDegrees.Display();
         }
     }
 }
---
+++ karolczuk_c#_parallel_concurent_async/ProcessingAsync/SineCosineDegrees.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProcessingAsync
+{
+	class SineCosineDegrees
+	{
+
+        public static async Task Display()
+        {
+            var ca1 = Task.Run(SineAngleConsole);
+            var ca2 = Task.Run(CosineAngleConsole);
+
+            for (var i = 2; i < 10; i++)
+            {
+                Thread.Sleep(i * 335);
+                Console.WriteLine("...");
+            }
+
+            await ca1;
+            await ca2;
+        }
+
+        public static void CosineAngleConsole()
+        {
+            for (var i = 0; i < 61; i++)
+            {
+                Thread.Sleep(270);
+                Console.WriteLine($" .. cos {i}* = {Math.Cos((i * Math.PI) / 180)}");
+            };
+
+        }
+
+        public static void SineAngleConsole()
+        {
+            for (var i = 0; i < 61; i++)
+			{
+                Thread.Sleep(310);
+                Console.WriteLine($"  . sin {i}* = {Math.Sin((i * Math.PI) / 180)}");
+            };
+
+        }
+    }
+}
