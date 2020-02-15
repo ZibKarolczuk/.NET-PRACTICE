b550369503f3c79ba1d6f8c92f5c525fc984359f
Zbigniew Karolczuk <karolczuk.z@gmail.com>
2020-02-15 22:05:59 +0100

    First glimpse on Parallel Programming

--- karolczuk_c#_parallel_concurent_async/ProcessingParallel/Program.cs
+++ karolczuk_c#_parallel_concurent_async/ProcessingParallel/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ProcessingParallel
 {
@@ -6,7 +9,15 @@ namespace ProcessingParallel
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
-    }
+            Console.WriteLine("Hello Parallel Programming World!\n");
+
+            var angles = new List<int>() { 0, 30, 45, 60, 90, 120, 135, 150, 180, 210, 225, 240, 270, 300, 315, 330, 360 };
+
+            Parallel.ForEach<int>(angles, new ParallelOptions { MaxDegreeOfParallelism = 3 }, angle =>
+            {
+                Console.WriteLine($" cos {angle}* = {Math.Cos((angle * Math.PI) / 180)}");
+				Thread.Sleep(5000);
+            });
+		}
+	}
 }
