7a1c51881e2bc0ff380c4ea2665c6b07b50e37f0
Zbigniew Karolczuk <karolczuk.z@gmail.com>
2020-02-15 22:25:48 +0100

    Parallel example refactor

---
+++ karolczuk_c#_parallel_concurent_async/ProcessingParallel/ParallelSimple.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProcessingParallel
+{
+	public static class ParallelSimple
+	{
+		public static void Display() {
+			var angles = new List<int>() { 0, 30, 45, 60, 90, 120, 135, 150, 180, 210, 225, 240, 270, 300, 315, 330, 360 };
+			Parallel.ForEach<int>(angles, new ParallelOptions { MaxDegreeOfParallelism = 3 }, angle =>
+				{
+					Console.WriteLine($" cos {angle}* = {Math.Cos((angle * Math.PI) / 180)}");
+					Thread.Sleep(5000);
+				});
+		}
+	}
+}
--- karolczuk_c#_parallel_concurent_async/ProcessingParallel/Program.cs
+++ karolczuk_c#_parallel_concurent_async/ProcessingParallel/Program.cs
@@ -11,13 +11,7 @@ namespace ProcessingParallel
         {
             Console.WriteLine("Hello Parallel Programming World!\n");
 
-            var angles = new List<int>() { 0, 30, 45, 60, 90, 120, 135, 150, 180, 210, 225, 240, 270, 300, 315, 330, 360 };
-
-            Parallel.ForEach<int>(angles, new ParallelOptions { MaxDegreeOfParallelism = 3 }, angle =>
-            {
-                Console.WriteLine($" cos {angle}* = {Math.Cos((angle * Math.PI) / 180)}");
-				Thread.Sleep(5000);
-            });
+            ParallelSimple.Display();
 		}
 	}
 }
